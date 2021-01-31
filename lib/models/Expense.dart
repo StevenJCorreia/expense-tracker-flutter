34bf863c92377ea0c2e5c2078542935b2e9b9bd9
Steven Correia <scorreia@ips-yes.com>
2021-01-31 13:09:47 -0500

    Added import and export features

--- lib/models/Expense.dart
+++ lib/models/Expense.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:expense_tracker/models/Category.dart';
 
 /// Expense data model
@@ -16,21 +18,102 @@ class Expense {
     this.price = 0;
   }
 
-  String toString() {
+  String toJSON() {
     return 'id: $id, category: {${category.toString()}}, date: ${date.toString()}, description: "$description", price: $price';
   }
 
-  factory Expense.fromJSON(Map<String, dynamic> json) => Expense.builder(
-        id: json['id'],
-        category: Category.builder(
-          id: json['category_id'],
-          name: json['name'],
-        ),
-        date: DateTime.parse(json['date']),
-        description: json['description'],
-        price: json['price'],
-      );
+  String toCSV() {
+    return '${this.category.name},${this.date.toString()},${this.price},"$description"';
+  }
+
+  factory Expense.fromJSON(Map<String, dynamic> json) {
+    return Expense.builder(
+      id: json['id'],
+      category: Category.builder(
+        id: json['category_id'],
+        name: json['name'],
+      ),
+      date: DateTime.parse(json['date']),
+      description: json['description'],
+      price: json['price'],
+    );
+  }
+
+  factory Expense.fromCSV(String line) {
+    final List<String> values = line.split(',');
+    String category = values[0];
+    DateTime date = DateTime.parse(values[1]);
+    num price = num.parse(values[2]);
+    String description = '';
+
+    // Get description value
+    for (int i = 0; i < values.length; i++) {
+      if (i >= 0 && i <= 2) {
+        continue;
+      }
+
+      description += values[i];
+    }
+
+    // Trim CSV escape quotes off value
+    description.replaceRange(0, 1, '');
+    description.replaceRange(description.length - 1, description.length, '');
+
+    return Expense.builder(
+      id: -1,
+      category: Category.builder(id: -1, name: category),
+      date: date,
+      description: description,
+      price: price,
+    );
+  }
 
   Expense.builder(
       {this.id, this.category, this.date, this.description, this.price});
+
+  static bool exportExpenses(String directory, List<Expense> expenses) {
+    bool result = true;
+
+    final String fileName = 'ExpenseTracker_${DateTime.now().toString()}';
+    File file = File('$directory/$fileName.csv');
+
+    String contents = 'Category,Date,Price,Description';
+    for (var expense in expenses) {
+      contents += '\n${expense.toCSV()}';
+    }
+
+    try {
+      file.writeAsStringSync(contents);
+    } on FileSystemException {
+      result = false;
+    }
+
+    return result;
+  }
+
+  static List<Expense> importExpenses(String location) {
+    List<Expense> expenses = [];
+
+    File file = File(location);
+
+    try {
+      List<String> lines = file.readAsLinesSync();
+
+      // Remove legend
+      lines.removeAt(0);
+
+      for (int i = 0; i < lines.length; i++) {
+        Expense temp = Expense.fromCSV(lines[i]);
+
+        // Assign fake ID for model listview
+        temp.id = -i;
+
+        expenses.add(temp);
+      }
+    } catch (e) {
+      throw e;
+    }
+
+    return expenses;
+  }
 }
